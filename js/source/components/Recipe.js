d19b60cd80b8e8d214a36a4f8bd9b988412db5f8
Chris Cantu <pinion31@gmail.com>
2017-05-20 01:09:22 -0500

    Initial App Setup

---
+++ js/source/components/Recipe.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Component, PropTypes} from 'react';
+
+class Recipe extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Recipe
---
+++ js/source/components/RecipeHolder.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Component, PropTypes} from 'react';
+
+class RecipeHolder extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return(
+    );
+  }
+
+}
+
+export default RecipeHolder
